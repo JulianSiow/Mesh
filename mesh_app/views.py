76479a07478246af61f8fbfe9351970aba37f88f
unknown <anndymo@gmail.com>
2019-11-06 11:57:17 -0800

    create profile and event views

--- mesh_app/views.py
+++ mesh_app/views.py
@@ -4,3 +4,29 @@ from django.contrib.auth.decorators import login_required
 
 from .models import User, Event
 # Create your views here.
+
+def profile_create(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            profile = form.save()
+            return redirect('____', pk=profile.pk)
+    else:
+        form = ProfileForm()
+    context = {'form': form}
+    return render(request, '____', context)
+
+def event_create(request):
+    user = User.object.get(id=pk)
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valud():
+            event = form.save(commit=False)
+            event.creator = user
+            return redirect('____', pk = event.pk)
+        else:
+            form = EventForm()
+        context = {'form': form, 'header':"Add an Event!"}
+        return render(request,'____', context)
+
+
