86983e2114febc5881ffe6056205d922fdcd44f7
JSiow <julian.k.siow@gmail.com>
2019-11-06 13:55:00 -0800

    adds form to create event

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+from mesh_app.models import Profile
 
 # Create your views here.
+
+def register(req):
+    if req.method == 'POST':
+        first_name = req.POST['first_name']
+        last_name = req.POST['last_name']
+        username = req.POST['username']
+        email = req.POST['email']
+        password = req.POST['password']
+        password2 = req.POST['password2']
+        prof_pic = req.POST['prof_pic']
+        field = req.POST['field']
+        portfolio_link = req.POST['portfolio_link']
+        linkedin = req.POST['linkedin']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                context = {'error':'Username is already taken'}
+                return render(req, '')
