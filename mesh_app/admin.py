4ac98f511bac1c4e6fec8beabc1c8b33bcc46495
JSiow <julian.k.siow@gmail.com>
2019-11-07 13:21:53 -0800

    exports account urls

--- mesh_app/admin.py
+++ mesh_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Event, Profile, Attendees
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Profile)
+admin.site.register(Attendees)
--- mesh_project/urls.py
+++ mesh_project/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mesh_app.urls')),
+    path('', include('accounts.urls')),
 ]
