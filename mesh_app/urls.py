21784f3ea854c1ed46292231d20b42c3cf5fb39d
hlalefar <hlalefar@gmail.com>
2019-11-07 13:56:43 -0800

    submits base structure, extends to other pages

--- mesh_app/urls.py
+++ mesh_app/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('user/<int:pk>/edit/', views.profile_edit, name='profile_edit'),
     #path('user/<int:pk>/delete/', views.user_delete, name='user_delete'),
     path('user/<int:pk>/event/new/', views.event_create, name='event_create'),
-    # path('event/', views.event),
     # path('user/<int:pk>/event/edit/', views_event_edit, name='event_edit'),
+    # path('event/', views.event),
+    # path('event/<int:event_pk>/', views.event_detail, name='event_detail'),
     # path('user/<int:pk>/event/delete/', views_event_delete, name='event_delete')
     # path('user/<int:pk>/event/join', views.event_join, name='event_join')
 ]
