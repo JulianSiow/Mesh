5f843fd3059c7631b9f81ff392e7920e9a5a7f76
JSiow <julian.k.siow@gmail.com>
2019-11-06 16:27:44 -0800

    commit to pull

--- mesh_app/urls.py
+++ mesh_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('user/<int:pk>/edit/', views.user_edit, name='user_edit'),
     path('user/<int:pk>/delete/', views.user_delete, name='user_delete'),
     path('user/<int:pk>/event/new/', views.event_create, name='event_create'),
-    path('event/', views.event),
+    path('event/', views.event, name='event'),
     path('user/<int:pk>/event/edit/', views_event_edit, name='event_edit'),
     path('user/<int:pk>/event/delete/', views_event_delete, name='event_delete')
 ]
